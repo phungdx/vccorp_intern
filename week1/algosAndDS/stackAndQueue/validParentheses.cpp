f74539252a4ab8e9debed210e47092dbbb41eb55
phungdx <phungdaoxuanbkhn@gmail.com>
2022-04-15 22:52:00 +0700

    modified

--- week1/algosAndDS/stackAndQueue/validParentheses.cpp
+++ week1/algosAndDS/stackAndQueue/validParentheses.cpp
@@ -5,32 +5,62 @@ using namespace std;
 
 class Stack{
     private:
-        constexpr static int size = 30;
+        // Kích thước tối đa của stack
+        constexpr static int size = 10;
+
+        // Track vị trí top của stack
         int top_index = -1;
-        char A[size];
+
+        // Mảng mô phỏng stack
+        int A[size];
 
     public:
-        void push(char x){
+        // Check xem stack có rỗng hay không
+        bool isEmpty(){
+            if(top_index < 0){
+                return true;
+            }
+            return false;
+        }
+
+        // Check xem stack đã đầy chưa
+        bool isFull(){
+            if(top_index == size - 1){
+                return true;
+            }
+            return false;
+        }
+
+
+        // Thêm phần tử vào stack
+        void push(int x){
+            // nếu stack đầy thì dừng
             if(isFull()){
                 cout << "Stack is full" << '\n';
                 return;
             }
 
+            // Tăng chỉ số top lên 1 và thêm giá trị mới vào
             top_index += 1;
             A[top_index] = x;
         }
 
 
+        // Xóa 1 phần tử top của stack
         void pop(){
+            // Nếu stack rỗng thì dừng
             if(isEmpty()){
                 cout << "Stack is empty"<< '\n';
                 return;
             }
 
+            // Giảm chỉ số của top đi 1 thì sẽ xóa nó khỏi stack
             top_index -= 1;
         }
 
+        // Lấy giá trị đỉnh stack
         int top(){
+            // Nếu stack rỗng thì dừng
             if(isEmpty()){
                 cout << "Stack is empty"<< '\n';
                 return -1;
@@ -39,65 +69,61 @@ class Stack{
             return A[top_index];
         }
 
-        bool isEmpty(){
-            if(top_index < 0){
-                return true;
-            }
-            return false;
-        }
-
-        bool isFull(){
-            if(top_index == size - 1){
-                return true;
-            }
-            return false;
-        }
-
+        // In stack ra màn hình
         void print(){
             if(isEmpty()){
                 cout << "Stack is empty"<<'\n';
                 return;
             }
             for (int i = 0; i <= top_index; ++i){
-                cout << A[i];
+                cout << A[i] << " ";
             }
             cout << endl;
-        }
-
-        int getSize(){
-            return top_index + 1;
-        }
-
-        void pushMultiple(char s[]){
-            int n = strlen(s);
-            for (int i = 0; i < n; ++i){
-                push(s[i]);
-            }
         }
 };
 
 
+
+/* Kiểm tra tính hợp lệ của 1 string chứa toàn các dấu mở và đóng ngoặc
+
+Quy tắc tiên quyết:
+    - Loại dấu ngoặc nào mở ra cuối cùng thì sẽ phải đóng vào đầu tiên.
+
+*/
 bool isValid(char s[]){
+    // Lấy length của string input
     int n = strlen(s);
+
+    // Khởi tạo 1 stack để chứa các dấu mở ngoặc
     Stack myStack;
 
     for(int i = 0; i < n; ++i){
+        // lần lượt đưa các dấu mở ngoặc vào stack
         if(s[i] == '(' || s[i] == '{' || s[i] == '['){
             myStack.push(s[i]);
         }
+
+        // Nếu gặp phải dấu đóng ngoặc
         else if(s[i] == ')' || s[i] == '}' || s[i] == ']'){
+            // Kiểm tra xem stack có rỗng không, nếu rỗng --> string input không hợp lệ
             if(myStack.isEmpty()){
                 return false;
             }
+
+            // Nếu stack không rỗng
             else{
+                // Lấy giá trị top của stack
                 char top = myStack.top();
+
+                // Kiểm tra xem dấu mở ra cuối cùng và dấu đóng vào mới nhất có cùng loại không
                 if(s[i] == ')' && top == '(' ||
                     s[i] == '}' && top == '{' ||
                     s[i] == ']' && top == '['){
-
+                        // Nếu cùng loại thì hợp lệ, xóa top của stack đi để duyệt tiếp
                         myStack.pop();
                     }
 
+                // Nếu không cùng loại thì ---> string input không hợp lệ
                 else{
                     return false;
                 }
@@ -105,6 +131,8 @@ bool isValid(char s[]){
         }
     }
 
+    // sau khi duyệt xong string input mà stack của ta rỗng thì chứng tỏ string input hợp lệ,
+    // và ngược lại
     return myStack.isEmpty() ? true:false;
 }
 
