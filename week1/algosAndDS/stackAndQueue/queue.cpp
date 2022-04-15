aca617235be55f93d4864a1ec27092839ab85e39
phungdx <phungdaoxuanbkhn@gmail.com>
2022-04-15 22:39:41 +0700

    modified

--- week1/algosAndDS/stackAndQueue/queue.cpp
+++ week1/algosAndDS/stackAndQueue/queue.cpp
@@ -1,21 +1,39 @@
 #include<iostream>
 using namespace std;
 
+/*
+Queue sẽ được biểu diễn như sau
+
+            ---------------------------
+            front                   rear
+            ---------------------------
+            Ra                      Vào
+
+*/
 class Queue{
     private:
+        // Kichs thước tối đa queue
         constexpr static int size = 10;
+
+        // Track vị trí đầu vào của queue
         int rear = -1;
+
+        // Track vị trí đầu ra của queue
         int front = -1;
+
+        // Khởi tạo mảng để mô phỏng queue
         int A[size];
 
     public:
+        // Kiểm tra queue có rỗng không
         bool isEmpty(){
-            if(front == -1 && rear == -1){
+            if((front == -1 && rear == -1) || front > rear){
                 return true;
             }
             return false;
         }
 
+        // Kiểm tra queue có đầy không
         bool isFull(){
             if(rear == size - 1){
                 return true;
@@ -23,32 +41,43 @@ class Queue{
             return false;
         }
 
+        // Thêm phần tử vào queue
         void enqueue(int x){
+            // Nếu queue đầy thì dừng
             if(isFull()){
                 cout <<"Queue is full"<<'\n';
                 return;
             }
 
+            // Nếu queue rỗng thì tăng chỉ số của front và rear lên 0
+            // để chỉ vào phần tử thứ nhất của queue
             if(isEmpty()){
                 front = rear = 0;
             }
 
+            // Nếu queue có phần tử rồi thì tăng chỉ số của rear lên 1
             else{
                 ++rear;
             }
 
+            // gán phần tử mới cho vị trí mới mà rear chỉ.
             A[rear] = x;
         }
 
+        // Xóa 1 phần tử ra khỏi queue
         void dequeue(){
+            // nếu queue rỗng thì ngừng
             if(isEmpty()){
                 cout <<"Queue is empty"<<'\n';
+
+                // Đặt lại chỉ số của front và rear về ban đầu
+                if(front > rear){
+                    front = rear = -1;
+                }
                 return;
             }
             
-            if(front == rear){
-                front = rear = -1;
-            }
+            // Tăng chỉ số front lên 1 là bỏ được phần tử đầu khỏi queue
             else{
                 ++front;
             }
@@ -67,7 +96,7 @@ class Queue{
         }
 
         int getSize(){
-            return rear - front;
+            return rear - front + 1;
         }
 
         int getFront(){
@@ -98,6 +127,6 @@ int main(){
     q.enqueue(99);
 
     
-    cout << "Size of queue:" <<q.getSize() <<'\n';
+    cout << "Size of queue:" << q.getSize() <<'\n';
     q.print();
 }
--- week1/algosAndDS/stackAndQueue/stack.cpp
+++ week1/algosAndDS/stackAndQueue/stack.cpp
@@ -4,32 +4,62 @@ using namespace std;
 
 class Stack{
     private:
+        // Kích thước tối đa của stack
         constexpr static int size = 10;
+
+        // Track vị trí top của stack
         int top_index = -1;
+
+        // Mảng mô phỏng stack
         int A[size];
 
     public:
+        // Check xem stack có rỗng hay không
+        bool isEmpty(){
+            if(top_index < 0){
+                return true;
+            }
+            return false;
+        }
+
+        // Check xem stack đã đầy chưa
+        bool isFull(){
+            if(top_index == size - 1){
+                return true;
+            }
+            return false;
+        }
+
+
+        // Thêm phần tử vào stack
         void push(int x){
+            // nếu stack đầy thì dừng
             if(isFull()){
                 cout << "Stack is full" << '\n';
                 return;
             }
 
+            // Tăng chỉ số top lên 1 và thêm giá trị mới vào
             top_index += 1;
             A[top_index] = x;
         }
 
 
+        // Xóa 1 phần tử top của stack
         void pop(){
+            // Nếu stack rỗng thì dừng
             if(isEmpty()){
                 cout << "Stack is empty"<< '\n';
                 return;
             }
 
+            // Giảm chỉ số của top đi 1 thì sẽ xóa nó khỏi stack
             top_index -= 1;
         }
 
+        // Lấy giá trị đỉnh stack
         int top(){
+            // Nếu stack rỗng thì dừng
             if(isEmpty()){
                 cout << "Stack is empty"<< '\n';
                 return -1;
@@ -38,20 +68,7 @@ class Stack{
             return A[top_index];
         }
 
-        bool isEmpty(){
-            if(top_index < 0){
-                return true;
-            }
-            return false;
-        }
-
-        bool isFull(){
-            if(top_index == size - 1){
-                return true;
-            }
-            return false;
-        }
-
+        // In stack ra màn hình
         void print(){
             if(isEmpty()){
                 cout << "Stack is empty"<<'\n';
